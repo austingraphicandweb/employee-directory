85b598cc6381d5ebc1c97a32ab6289594f981f3b
Austin Keener <austingraphicandweb@gmail.com>
2020-08-03 19:18:29 -0400

    removal of console.logs and addition of npm package to package.json so that gh pages works:

--- src/components/EmployeeTable.jsx
+++ src/components/EmployeeTable.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 
 function EmployeeTable({employees, employeeSorted}) {
-    console.log(employeeSorted);
     return (
         <div className="EmployeeTable">
             <table>
