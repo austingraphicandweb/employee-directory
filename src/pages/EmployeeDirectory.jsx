d1e3c7ddf7ce06b3e70758aa2ec0e5f2f66d638c
Austin Keener <austingraphicandweb@gmail.com>
2020-08-03 18:35:32 -0400

    filter by phone enabled

--- src/pages/EmployeeDirectory.jsx
+++ src/pages/EmployeeDirectory.jsx
@@ -15,8 +15,8 @@ class EmployeeDirectory extends Component {
     }
 
     employeeFilter(value) {
-        //starting at the 0 character i am going the length of what I type in and comparing it to the data which will be returned in the new array
-        const employees = this.state.employees.filter(employee => employee.name.slice(0, value.length).toLowerCase() === value.toLowerCase() || employee.department.slice(0, value.length).toLowerCase() === value.toLowerCase());
+        //starting at the 0 character i am going the length of what I type in and comparing it to the data which will be returned in the new array. it filters by nam
+        const employees = this.state.employees.filter(employee => employee.name.slice(0, value.length).toLowerCase() === value.toLowerCase() || employee.email.slice(0, value.length).toLowerCase() === value.toLowerCase() || employee.phone.slice(0, value.length).toLowerCase() === value.toLowerCase() || employee.department.slice(0, value.length).toLowerCase() === value.toLowerCase());
         this.setState({ filteredEmployees: employees });
     }
     employeeSorted = (property) => {
